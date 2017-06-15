a54b37f5053546ed51fae76d81e9f3326eab7cc4
johnmildinhall <john_mildinhall@hotmail.com>
2017-06-15 14:14:45 +0100

    Mugshots (#8)
    * Added module to give random mugshot URL of specified gender
    
    * tidied function names
    
    * Added image to the view to show it works :)
    
    * trying again.

---
+++ app/assets/data/mugshot.js
@@ -0,0 +1,64 @@
+//Module to return random offender image URL
+
+exports.getImage = function(gender){
+	
+	//Create 2 objects, one with 5 male mugshots, one with 5 female.
+	var imageDataMale = 
+		[{
+				"gender":"male"
+			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/male-1.png"	
+		},
+		{
+				"gender":"male"
+			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/male-2.png"	
+		},	{
+				"gender":"male"
+			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/male-3.png"	
+		},	{
+				"gender":"male"
+			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/male-4.png"	
+		},	{
+				"gender":"male"
+			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/male-5.png"	
+		}];
+
+	var imageDataFemale =	
+		[{
+				"gender":"female"
+			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/female-1.png"	
+		},
+		{
+				"gender":"female"
+			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/female-2.png"	
+		},
+		,{
+				"gender":"female"
+			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/female-3.png"	
+		},
+		{
+				"gender":"female"
+			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/female-4.png"	
+		},
+		{
+				"gender":"female"
+			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/female-5.png"	
+		}];
+
+		var eitherGender = imageDataMale.concat(imageDataFemale);
+	
+	//Return random image, given gender. If no gender given, return random image of either gender
+	switch (gender){
+		case "male":
+			return imageDataMale[Math.floor(Math.random()*imageDataMale.length)];
+			break;
+		case "female":
+			return imageDataFemale[Math.floor(Math.random()*imageDataFemale.length)];
+			break;
+		default:
+			return eitherGender[Math.floor(Math.random()*eitherGender.length)];
+			break;
+	}
+		
+	
+
+}
