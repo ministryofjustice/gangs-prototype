7db06c667fa275aa194651f962bd27bf2ac10210
Clive Murray <hi+github@clivemurray.com>
2017-06-27 15:02:58 +0100

    make navigation next and prev work again (#31)
    merging this one without review as it's just a quick fix

--- app/modules/navigation.js
+++ app/modules/navigation.js
@@ -2,7 +2,7 @@
 
 var navigation = {
   next: function(current, max) {
-    n = parseInt(current, 10) + 1;
+    var n = parseInt(current, 10) + 1;
     if(n > max - 1) {
       n = 0;
     }
@@ -10,7 +10,7 @@ var navigation = {
     return n;
   },
   prev: function(current, max) {
-    current = parseInt(n, 10) - 1;
+    var n = parseInt(current, 10) - 1;
     if(n < 0) {
       n = max - 1;
     }
