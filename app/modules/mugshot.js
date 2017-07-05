a73e369c72cc51c3f7a4449721f2e07735cd274a
Clive Murray <hi+github@clivemurray.com>
2017-07-05 10:25:34 +0100

    Store mugshots in repo, add more (#49)
    * store mugshots in repo, add more
    
    * fix avatars on search results

--- app/modules/mugshot.js
+++ app/modules/mugshot.js
@@ -2,63 +2,31 @@
 
 exports.getImage = function(gender){
 
-	//Create 2 objects, one with 5 male mugshots, one with 5 female.
-	var imageDataMale =
-		[{
-				"gender":"male"
-			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/male-1.png"
-		},
-		{
-				"gender":"male"
-			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/male-2.png"
-		},	{
-				"gender":"male"
-			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/male-3.png"
-		},	{
-				"gender":"male"
-			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/male-4.png"
-		},	{
-				"gender":"male"
-			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/male-5.png"
-		}];
+  var mugshots = {
+    male: 15,
+    female: 15
+  };
 
-	var imageDataFemale =
-		[{
-				"gender":"female"
-			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/female-1.png"
-		},
-		{
-				"gender":"female"
-			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/female-2.png"
-		},
-		{
-				"gender":"female"
-			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/female-3.png"
-		},
-		{
-				"gender":"female"
-			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/female-4.png"
-		},
-		{
-				"gender":"female"
-			,	"url":"https://s3-eu-west-1.amazonaws.com/gangmatronimages/female-5.png"
-		}];
+  function makeFilename(gender) {
+    gender = gender || (Math.random() < 0.5 ? 'male' : 'female');
 
-		var eitherGender = imageDataMale.concat(imageDataFemale);
+    return {
+      gender: gender,
+      filename: gender + '/' + (Math.floor(Math.random() * mugshots[gender]) + 1) + '.png'
+    };
+  }
 
-	//Return random image, given gender. If no gender given, return random image of either gender
-	switch (gender){
-		case "male":
-			return imageDataMale[Math.floor(Math.random()*imageDataMale.length)];
-			break;
-		case "female":
-			return imageDataFemale[Math.floor(Math.random()*imageDataFemale.length)];
-			break;
-		default:
-			return eitherGender[Math.floor(Math.random()*eitherGender.length)];
-			break;
-	}
-
-
+  //Return random image, given gender. If no gender given, return random image of either gender
+  switch (gender){
+    case "male":
+      return makeFilename('male');
+      break;
+    case "female":
+      return makeFilename('female');
+      break;
+    default:
+      return makeFilename();
+      break;
+  }
 
 }
